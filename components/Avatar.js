fbab75e8bd8cb3a5daada9b6f3dbf199b4e8c587
Manuel Gonzalez <manuelg9704@gmail.com>
2023-09-30 16:00:15 -0500

    videeooo

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import ReactPlayer from 'react-player';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+// Importa ReactPlayer solo en el lado del cliente
+const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 const Avatar = () => {
-  const [volume, setVolume] = useState(0.8); // inicializa el volumen al 80%
+  const [volume, setVolume] = useState(0.8);
 
-  // función para manejar el cambio de volumen
   const handleVolumeChange = (e) => setVolume(parseFloat(e.target.value));
 
   return (
     <div className="relative w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]">
       <ReactPlayer
         url='/videomanuel.mp4'
-        playing={true} // auto-play
-        controls={true} // muestra los controles del reproductor
-        volume={volume} // establece el volumen del reproductor
+        playing={true}
+        controls={true}
+        volume={volume}
         width='100%'
         height='100%'
       />
-      {/* Control deslizante para el volumen */}
       <input 
         type="range" 
         value={volume} 
