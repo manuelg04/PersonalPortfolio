8bac4fe242f7cd5f3dec2a47202ee7ecb6ad78de
Manuel Gonzalez <manuelg9704@gmail.com>
2023-09-30 16:03:33 -0500

    react

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,32 +1,21 @@
-import dynamic from 'next/dynamic';
-import { useState } from 'react';
-
-// Importa ReactPlayer solo en el lado del cliente
-const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 const Avatar = () => {
-  const [volume, setVolume] = useState(0.8);
+  const [playing, setPlaying] = useState(false);
 
-  const handleVolumeChange = (e) => setVolume(parseFloat(e.target.value));
+  useEffect(() => {
+    setPlaying(true);
+  }, []);
 
   return (
-    <div className="relative w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]">
+    <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]">
       <ReactPlayer
         url='/videomanuel.mp4'
-        playing={true}
+        playing={playing}
         controls={true}
-        volume={volume}
         width='100%'
         height='100%'
-      />
-      <input 
-        type="range" 
-        value={volume} 
-        min="0" 
-        max="1" 
-        step="0.01" 
-        onChange={handleVolumeChange} 
-        className="absolute bottom-0 left-0 w-full opacity-50"
       />
     </div>
   );
