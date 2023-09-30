771e0ba7b3aa8c27a989433a741ccc034a8418fb
Manuel Gonzalez <manuelg9704@gmail.com>
2023-09-30 15:58:47 -0500

    video

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+
 const Avatar = () => {
+  const [volume, setVolume] = useState(0.8); // inicializa el volumen al 80%
+
+  // función para manejar el cambio de volumen
+  const handleVolumeChange = (e) => setVolume(parseFloat(e.target.value));
+
   return (
-    <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]"> 
-      {/* Estilos temporales para diagnóstico y dimensiones específicas para diferentes tamaños de pantalla */}
-      <video
-        src={'/videomanuel.mp4'}
-        controls
-        muted
-        autoPlay
-        playsInline
-        className="w-full h-full" 
-      >
-        Tu navegador no soporta el tag de video.
-      </video>
+    <div className="relative w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]">
+      <ReactPlayer
+        url='/videomanuel.mp4'
+        playing={true} // auto-play
+        controls={true} // muestra los controles del reproductor
+        volume={volume} // establece el volumen del reproductor
+        width='100%'
+        height='100%'
+      />
+      {/* Control deslizante para el volumen */}
+      <input 
+        type="range" 
+        value={volume} 
+        min="0" 
+        max="1" 
+        step="0.01" 
+        onChange={handleVolumeChange} 
+        className="absolute bottom-0 left-0 w-full opacity-50"
+      />
     </div>
   );
 };
