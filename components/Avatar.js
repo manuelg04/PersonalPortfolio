a6178d5a29b9301cd2638793bd855a8663647f68
Manuel Gonzalez <manuelg9704@gmail.com>
2023-09-30 15:08:13 -0500

    video

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,14 +3,18 @@ import Image from 'next/image';
 
 const Avatar = () => {
   return (
-    <div className='hidden xl:flex xl:max-w-none w-64'>
-      <Image
-        src={'/king.jpg'}
+    <div className='hidden xl:flex xl:max-w-none w-128'>
+      <video
+        src={'/videomanuel.mp4'}
         width={737}
         height={678}
-        alt=''
+        controls
+        muted
+        autoPlay
         className='translate-z-0 w-full h-full border-4 border-white rounded-full shadow-2xl'
-      />
+      >
+        Tu navegador no soporta el tag de video.
+      </video>
     </div>
   );
 };
